7c20536d183a240b84c6ed77d76ff7efb7e209be
Chauhan Yashkumar  Dilipbhai <152592301+yashchauhan040205@users.noreply.github.com>
2026-02-07 19:43:43 +0530

    Add files via upload

---
+++ DappToken16Sale.js
@@ -0,0 +1,84 @@
+var DappToken16Sale = artifacts.require("./DappToken16Sale.sol");
+var DappToken16 = artifacts.require("./DappToken16.sol");
+
+contract('DappToken16Sale', function(accounts) {
+    var tokenInstance;
+    var tokenSaleInstance;
+    var tokenPrice = 1000000000000000; // in wei = 0.001 ETH
+    var admin = accounts[0];
+    var buyer = accounts[1];
+    var tokensAvailable = 750000;
+    var numberOfTokens;
+    it('initializes the contract with the correct values', function() {
+        return DappToken16Sale.deployed().then(function(instance) {
+            tokenSaleInstance = instance;
+            return tokenSaleInstance.address;
+        }).then(function(address) {
+            assert.notEqual(address, 0x0, 'has contract address');
+            return tokenSaleInstance.tokenContract();
+        }).then(function(address) {
+            assert.notEqual(address, 0x0, 'has token contract address');
+            return tokenSaleInstance.tokenPrice();
+        }).then(function(price) {
+            assert.equal(price, tokenPrice, 'token price is correct');
+        });
+    });
+
+    it('facilitates token buying', function() {
+        return DappToken16.deployed().then(function(instance) { 
+            tokenInstance = instance;
+            return DappToken16Sale.deployed();
+        }).then(function(instance) {
+            tokenSaleInstance = instance;
+            return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin });
+        }).then(function(receipt) {
+            numberOfTokens = 10;
+            var value = numberOfTokens * tokenPrice;
+            return tokenSaleInstance.buyTokens(
+                numberOfTokens,
+                { from: buyer, value: value }
+            );
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+            assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account the tokens are purchased by');
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+            return tokenSaleInstance.tokensSold();
+        }).then(function(amount) {
+            assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold')
+            return tokenInstance.balanceOf(buyer);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), numberOfTokens);
+            return tokenInstance.balanceOf(tokenSaleInstance.address);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+            return tokenSaleInstance.buyTokens(
+                numberOfTokens,
+                { from: buyer, value: 1}
+            );
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+            return tokenSaleInstance.buyTokens(800000, { from: buyer, value: 800000 * tokenPrice });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot purchase more tokens than available');
+        });
+    });
+
+    it('ends token sale', function() {
+        return DappToken16.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return DappToken16Sale.deployed();
+        }).then(function(instance) {
+            tokenSaleInstance = instance;
+            return tokenSaleInstance.endSale({ from: buyer });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'must be admin to end sale');
+            return tokenSaleInstance.endSale({ from: admin });
+        }).then(function(receipt) {
+            return tokenInstance.balanceOf(admin);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 999990, 'returns all unsold dapp tokens to admin');
+            // Note: Can't check tokenPrice after selfdestruct - contract is destroyed
+        });
+    })  
+});
